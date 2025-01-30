6fd08b1e7279c17fd1141bc9090c30af3a6e7480
Ahmed Fawzy <142937094+hix-06@users.noreply.github.com>
2025-01-30 22:25:14 +0200

    Delete repo/DelegatesConsoleApp directory

--- repo/DelegatesConsoleApp/DelegatesDefinition.cs
+++
@@ -1,7 +0,0 @@
-
-namespace DelegatesConsoleApp
-{
-        public delegate void GreetDelegate(string name);
-        public delegate int MathOperation(int x, int y);
-        public delegate void Notify(string message);
-}
